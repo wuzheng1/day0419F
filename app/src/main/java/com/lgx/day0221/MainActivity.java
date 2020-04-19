2a8b6806dbb68181442535f6733b70c0de6291c3
wuzheng1 <2470290316@qq.com>
2020-04-20 00:04:06 +0800

    添加int

--- app/src/main/java/com/lgx/day0221/MainActivity.java
+++ app/src/main/java/com/lgx/day0221/MainActivity.java
@@ -16,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        int a = 0;
        /* ArrayList<Student> students = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             Student student = new Student();
