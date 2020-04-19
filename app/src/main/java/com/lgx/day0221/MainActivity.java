743c89d50a0e90ca94f3448e5c6b7a0831ce0d8d
wuzheng1 <2470290316@qq.com>
2020-04-19 23:58:06 +0800

    首次提交

---
+++ app/src/main/java/com/lgx/day0221/MainActivity.java
@@ -0,0 +1,56 @@
+package com.lgx.day0221;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+       /* ArrayList<Student> students = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Student student = new Student();
+            student.setName("李桂啸"+i);
+            student.setClasses("Java"+i);
+            students.add(student);
+        }
+
+         Iterator<Student> iterator = students.iterator();
+        while (iterator.hasNext()){
+            Student next = iterator.next();
+            Log.d("lili", "onCreate: "+next.getName()+next.getClasses());
+        }*/
+
+
+        HashSet<Student> students = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            Student student = new Student();
+            student.setName("李桂啸"+i);
+            student.setClasses("Java"+i);
+            students.add(student);
+        }
+
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Iterator<Student> iterator = students.iterator();
+        boolean b = iterator.hasNext();
+        while (b){
+            Student next = iterator.next();
+            Log.d("lili", "onCreate: "+next.getName()+next.getClasses());
+        }
+
+
+    }
+}
---
+++ app/src/main/java/com/lgx/day0221/Student.java
@@ -0,0 +1,24 @@
+package com.lgx.day0221;
+
+public class Student {
+
+    private String name;
+
+    private String classes;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getClasses() {
+        return classes;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+}
